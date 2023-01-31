0762d7bbfaf65eb47eb6f6d20b31f4f4218bbf93
Alex <gofavalon@gmail.com>
2023-01-31 16:43:59 +0400

    Versions+Doc

--- drf/Library/Library/urls.py
+++ drf/Library/Library/urls.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from app.views import AuthorModelViewSet, BiographyModelViewSet, BookModelViewSet, ArticleModelViewSet, MyAPIView
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Library',
+        default_version='1',
+        description='Doc for API',
+        contact=openapi.Contact(email='testemail@email.com'),
+        license=openapi.License(name='MIT License')
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
@@ -15,7 +30,15 @@ router.register('my', MyAPIView, basename='my')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('myapi/', MyAPIView.as_view({'get': 'list'})),
     path('api-auth/', include('rest_framework.urls')),
     path('api-token-auth/', obtain_auth_token),
+    # path('api/1/authors', include('authors.urls', namespace='1')),
+    # path('api/2/authors', include('authors.urls', namespace='2')),
+    # re_path(r'^myapi/(?P<version>\d)/authors/$', MyAPIView.as_view({'get': 'list'})),
+    path('api/authors', MyAPIView.as_view()),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
