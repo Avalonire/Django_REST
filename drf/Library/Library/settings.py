7f486176763170fb25ab7917cc1e18ae5a06f81a
Alex <gofavalon@gmail.com>
2023-02-06 15:57:56 +0400

    GraphQL

--- drf/Library/Library/settings.py
+++ drf/Library/Library/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'todo',
     'django_filters',
     'rest_framework.authtoken',
-    'drf_yasg'
+    'drf_yasg',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -146,4 +147,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
+}
+
+GRAPHENE = {
+    'SCHEMA': 'library.schema.schema'
 }
